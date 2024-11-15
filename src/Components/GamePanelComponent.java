20e3e9c2af7db108bfb3e1f37c0d2ef7ffee3991
Velur <aethillium@gmail.com>
2024-11-15 10:40:36 +0100

    gut progres

--- src/Components/GamePanelComponent.java
+++ src/Components/GamePanelComponent.java
@@ -103,7 +103,7 @@ public class GamePanelComponent extends JPanel implements Runnable{
         for(int i = 0; i < bugs.size(); i++){
             if(player.x >= bugs.get(i).x - 24 && player.x <= bugs.get(i).x + 24 && player.y >= bugs.get(i).y - 24 && player.y <= bugs.get(i).y + 24){
                 bugs.remove(i);
-                player.bodyParts.add(new PlayerBodyPart());
+                player.AddPlayerBodyPart();
             }
         }
         g2.dispose();
--- src/entities/Player.java
+++ src/entities/Player.java
@@ -78,12 +78,48 @@ public class Player extends Entity{
         }
     }
     
+    public void AddPlayerBodyPart(){
+        bodyParts.add(new PlayerBodyPart());
+        bodyParts.get(bodyParts.size() - 1).direction = bodyParts.get(bodyParts.size() - 2).direction;
+        switch(bodyParts.get(bodyParts.size() - 1).direction){
+            case "up":
+                bodyParts.get(bodyParts.size() - 1).y = bodyParts.get(bodyParts.size() - 2).y + gp.tileSize;
+                bodyParts.get(bodyParts.size() - 1).x = bodyParts.get(bodyParts.size() - 2).x;
+                System.out.println("Y przedostatniego  " + bodyParts.get(bodyParts.size() - 2).y);
+                break;
+            case "down":
+                bodyParts.get(bodyParts.size() - 1).y = bodyParts.get(bodyParts.size() - 2).y - gp.tileSize;
+                bodyParts.get(bodyParts.size() - 1).x = bodyParts.get(bodyParts.size() - 2).x;
+                System.out.println("Y przedostatniego  " + bodyParts.get(bodyParts.size() - 2).y);
+                break;
+            case "left":
+                bodyParts.get(bodyParts.size() - 1).y = bodyParts.get(bodyParts.size() - 2).y;
+                bodyParts.get(bodyParts.size() - 1).x = bodyParts.get(bodyParts.size() - 2).x + gp.tileSize;
+                System.out.println("Y przedostatniego  " + bodyParts.get(bodyParts.size() - 2).y);
+                break;
+            case "right":
+                bodyParts.get(bodyParts.size() - 1).y = bodyParts.get(bodyParts.size() - 2).y;
+                bodyParts.get(bodyParts.size() - 1).x = bodyParts.get(bodyParts.size() - 2).x - gp.tileSize;
+                System.out.println("Y przedostatniego  " + bodyParts.get(bodyParts.size() - 2).y);
+                break;
+        }
+    }
+    
     public void update(){
+        
+        switch(direction){
+            case "up" -> y -= speed;
+            case "down" -> y += speed;
+            case "left" -> x -= speed;
+            case "right" -> x += speed;
+        }
+        
         if(keyH.upPressed){
-            if(direction != "down"){   
-                if(direction != "up"){
+            if(!"down".equals(direction)){   
+                if(!"up".equals(direction)){
                     directionChangedSuccessfully = true; 
                     directionSwitchPoints.add(new SwitchDirectionPoint(x, y, direction, "up", false));
+                    cnt = 0;
                 }
                 prevDirection = direction;
                 direction = "up";
@@ -91,10 +127,11 @@ public class Player extends Entity{
             }
         }
         else if(keyH.downPressed){
-            if(direction != "up"){ 
-                if(direction != "down"){
+            if(!"up".equals(direction)){ 
+                if(!"down".equals(direction)){
                     directionChangedSuccessfully = true;  
                     directionSwitchPoints.add(new SwitchDirectionPoint(x, y, direction, "down", false));
+                    cnt = 0;
                 }
                 prevDirection = direction;
                 direction = "down";
@@ -102,10 +139,11 @@ public class Player extends Entity{
             }
         }
         else if(keyH.leftPressed){
-            if(direction != "right"){
-                if(direction != "left"){
+            if(!"right".equals(direction)){
+                if(!"left".equals(direction)){
                     directionChangedSuccessfully = true;   
                     directionSwitchPoints.add(new SwitchDirectionPoint(x, y, direction, "left", false));
+                    cnt = 0;
                 }
                 prevDirection = direction;
                 direction = "left";
@@ -113,10 +151,11 @@ public class Player extends Entity{
             }
         }
         else if(keyH.rightPressed){
-            if(direction != "left"){
-                if(direction != "right"){
+            if(!"left".equals(direction)){
+                if(!"right".equals(direction)){
                     directionChangedSuccessfully = true; 
                     directionSwitchPoints.add(new SwitchDirectionPoint(x, y, direction, "right", false));
+                    cnt = 0;
                 }
                 prevDirection = direction;
                 direction = "right";
@@ -124,38 +163,15 @@ public class Player extends Entity{
             }
         }
         
-        switch(direction){
-            case "up":
-                y -= speed;
-                break;
-            case "down":
-                y += speed;
-                break;
-            case "left":
-                x -= speed;
-                break;
-            case "right":
-                x += speed;
-                break;
-        }
-        
     }
     
     public void draw(Graphics2D g2){
         
         switch(direction){
-            case "up":
-                headImage = upHead;
-                break;
-            case "down":
-                headImage = downHead;
-                break;
-            case "left":
-                headImage = leftHead;
-                break;
-            case "right":
-                headImage = rightHead;
-                break;
+            case "up" -> headImage = upHead;
+            case "down" -> headImage = downHead;
+            case "left" -> headImage = leftHead;
+            case "right" -> headImage = rightHead;
         }
         
         drawHead(g2);
@@ -165,12 +181,12 @@ public class Player extends Entity{
     
     public void drawHead(Graphics2D g2){
         switch(direction){
-            case "up":
+            case "up" -> {
                 g2.setFont(new Font("Joystix Monospace", Font.PLAIN, 25));
                 g2.setColor(Color.CYAN);
                 g2.drawString(Integer.toString(bodyParts.size()), x + 45, y + 16);
-                break;
-            case "down":
+            }
+            case "down" -> {
                 g2.setFont(new Font("Joystix Monospace", Font.PLAIN, 25));
                 g2.setColor(Color.CYAN);
                 if(bodyParts.size() > 9){
@@ -179,24 +195,23 @@ public class Player extends Entity{
                 else{
                     g2.drawString(Integer.toString(bodyParts.size()), x - 20, y + 48);
                 }
-                break;
-            case "right":
+            }
+            case "right" -> {
                 g2.setFont(new Font("Joystix Monospace", Font.PLAIN, 25));
                 g2.setColor(Color.CYAN);
                 g2.drawString(Integer.toString(bodyParts.size()), x, y - 2);
-                break;
-            case "left":
+            }
+            case "left" -> {
                 g2.setFont(new Font("Joystix Monospace", Font.PLAIN, 25));
                 g2.setColor(Color.CYAN);
                 g2.drawString(Integer.toString(bodyParts.size()), x, y - 2);
-                break;
+            }
         }
         g2.drawImage(headImage, x, y, gp.tileSize, gp.tileSize, null);
     }
     
     public void drawBody(Graphics2D g2){
         //Podejście "zmierzaj ciałem do najbliższego switchPointa, a jeżeli wszystkie są już done to do głowy"
-
         //Sprawdzenie czy wszystkie switchPointy zostały obsłużone, jeżeli tak to podążaj za głową
         boolean followhead = true;
         for(int i = 0; i < directionSwitchPoints.size(); i++){
@@ -208,13 +223,14 @@ public class Player extends Entity{
         }
         
         if(followhead){
-            for(int i = 0; i < bodyParts.size() - 1; i++){
+            System.out.println("followHead");
+            for(int i = 0; i < bodyParts.size(); i++){
                 switch(direction){
-                    case "down":
+                    case "down" -> {
                         g2.drawImage(bodyParts.get(i).downImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
                         if(i != 0){
-                            bodyParts.get(i + 1).x = bodyParts.get(i).x;
-                            bodyParts.get(i + 1).y = bodyParts.get(i).y - gp.tileSize; 
+                            bodyParts.get(i).x = bodyParts.get(i - 1).x;
+                            bodyParts.get(i).y = bodyParts.get(i - 1).y - gp.tileSize + speed;
                         }  
                         else{
                             bodyParts.get(i).x = x;
@@ -222,12 +238,12 @@ public class Player extends Entity{
                             bodyParts.get(i + 1).x = bodyParts.get(i).x;
                             bodyParts.get(i + 1).y = bodyParts.get(i).y - gp.tileSize; 
                         }
-                        break;
-                    case "up":
+                    }
+                    case "up" -> {
                         g2.drawImage(bodyParts.get(i).upImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
                         if(i != 0){
-                            bodyParts.get(i + 1).x = bodyParts.get(i).x;
-                            bodyParts.get(i + 1).y = bodyParts.get(i).y + gp.tileSize; 
+                            bodyParts.get(i).x = bodyParts.get(i - 1).x;
+                            bodyParts.get(i).y = bodyParts.get(i - 1).y + gp.tileSize - speed;
                         }  
                         else{
                             bodyParts.get(i).x = x;
@@ -235,12 +251,12 @@ public class Player extends Entity{
                             bodyParts.get(i + 1).x = bodyParts.get(i).x;
                             bodyParts.get(i + 1).y = bodyParts.get(i).y + gp.tileSize; 
                         }
-                        break;
-                    case "left":
+                    }
+                    case "left" -> {
                         g2.drawImage(bodyParts.get(i).leftImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
                         if(i != 0){
-                            bodyParts.get(i + 1).x = bodyParts.get(i).x + gp.tileSize;
-                            bodyParts.get(i + 1).y = bodyParts.get(i).y; 
+                            bodyParts.get(i).x = bodyParts.get(i - 1).x + gp.tileSize - speed;
+                            bodyParts.get(i).y = bodyParts.get(i - 1).y;
                         }  
                         else{
                             bodyParts.get(i).x = x + gp.tileSize - speed;
@@ -248,12 +264,12 @@ public class Player extends Entity{
                             bodyParts.get(i + 1).x = bodyParts.get(i).x + gp.tileSize;
                             bodyParts.get(i + 1).y = bodyParts.get(i).y; 
                         }
-                        break;
-                    case "right":
+                    }
+                    case "right" -> {
                         g2.drawImage(bodyParts.get(i).rightImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
                         if(i != 0){
-                            bodyParts.get(i + 1).x = bodyParts.get(i).x - gp.tileSize;
-                            bodyParts.get(i + 1).y = bodyParts.get(i).y; 
+                            bodyParts.get(i).x = bodyParts.get(i - 1).x - gp.tileSize + speed;
+                            bodyParts.get(i).y = bodyParts.get(i - 1).y;
                         }  
                         else{
                             bodyParts.get(i).x = x - gp.tileSize + speed;
@@ -261,59 +277,44 @@ public class Player extends Entity{
                             bodyParts.get(i + 1).x = bodyParts.get(i).x - gp.tileSize;
                             bodyParts.get(i + 1).y = bodyParts.get(i).y; 
                         }
-                        break;
+                    }
                 }
             }
         }
         else{
             if(cnt <= gp.tileSize * bodyParts.size()){
+                System.out.println("cnt: " + cnt);
+                boolean oneTime = true;
                 for(int i = 0; i < directionSwitchPoints.size(); i++){
-                    for(int part = 0; part < bodyParts.size(); part++){                       
-                        if(bodyParts.get(part).x == directionSwitchPoints.get(i).x && bodyParts.get(part).y == directionSwitchPoints.get(i).y){
-                            bodyParts.get(part).direction = directionSwitchPoints.get(i).newDirection;
-                        }
-                        else{                           
-                            switch(bodyParts.get(part).direction){
-                                case "down":
-                                    bodyParts.get(part).y += speed;
-                                    break;
-                                case "up":
-                                    bodyParts.get(part).y -= speed;
-                                    break;
-                                case "left":
-                                    bodyParts.get(part).x -= speed;
-                                    break;
-                                case "right":
-                                    bodyParts.get(part).x += speed;
-                                    break;
+                    for(int j = 0; j < bodyParts.size(); j++){ 
+                        if(oneTime){
+                            switch(bodyParts.get(j).direction){
+                                case "down" -> bodyParts.get(j).y += speed;
+                                case "up" -> bodyParts.get(j).y -= speed;
+                                case "left" -> bodyParts.get(j).x -= speed;
+                                case "right" -> bodyParts.get(j).x += speed;
                             }
-                            
                         }
-
+                        
+                        if(bodyParts.get(j).x == directionSwitchPoints.get(i).x && bodyParts.get(j).y == directionSwitchPoints.get(i).y){
+                            bodyParts.get(j).direction = directionSwitchPoints.get(i).newDirection;
+                        }
+                    }
+                    oneTime = false;
+                    if((directionSwitchPoints.get(i).x == bodyParts.get(bodyParts.size() - 1).x) 
+                        && (directionSwitchPoints.get(i).y == bodyParts.get(bodyParts.size() - 1).y)){
+                           directionSwitchPoints.remove(i);
                     }
                 }
                 cnt += speed;
-                
-                for(int i = 0; i < bodyParts.size() - 1; i++){
+                for(int i = 0; i < bodyParts.size(); i++){
                     switch(bodyParts.get(i).direction){
-                        case "down":
-                            g2.drawImage(bodyParts.get(i).downImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
-                            break;
-                        case "up":
-                            g2.drawImage(bodyParts.get(i).upImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
-                            break;
-                        case "left":
-                             g2.drawImage(bodyParts.get(i).leftImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
-                             break;
-                        case "right":
-                             g2.drawImage(bodyParts.get(i).rightImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
-                             break;
+                        case "down" -> g2.drawImage(bodyParts.get(i).downImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
+                        case "up" -> g2.drawImage(bodyParts.get(i).upImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
+                        case "left" -> g2.drawImage(bodyParts.get(i).leftImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
+                        case "right" -> g2.drawImage(bodyParts.get(i).rightImage, bodyParts.get(i).x, bodyParts.get(i).y, gp.tileSize, gp.tileSize, null);
                     }
                 }
-            }
-            else{
-                directionSwitchPoints.get(0).done = true;
-                followhead = true;
             }
         }
     }
